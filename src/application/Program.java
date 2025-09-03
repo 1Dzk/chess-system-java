9cb04687645346a3fc6585ec4c0dd2d5a39ecad2
Henrique <162191143+1Dzk@users.noreply.github.com>
2025-09-03 11:26:03 -0300

    Project created

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,6 @@ package application;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.printf("Hello World!\n");
+        System.out.print("Hello World!\n");
     }
 }
